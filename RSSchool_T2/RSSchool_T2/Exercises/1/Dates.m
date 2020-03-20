6fef155ece8430f92fd9981e4d8d536d23673ff2
Anton <tatyana.shut.solveit@gmail.com>
2020-03-20 21:14:33 +0300

    feat: add Task1

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,23 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSCalendar *calendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    components.year = [year integerValue];
+    components.month = [month integerValue];
+    components.day = [day integerValue];
+    
+    if ([components isValidDateInCalendar:calendar]) {
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+        formatter.dateFormat = @"dd MMMM, EEEE";
+        NSDate *date = [calendar dateFromComponents:components];
+        return [formatter stringFromDate:date];
+        
+    } else {
+        return @"Такого дня не существует";
+    }
 }
 
 @end

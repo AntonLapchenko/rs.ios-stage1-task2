533883aff3547c9dfd0acde97f762ad451c1dd6a
Anton <tatyana.shut.solveit@gmail.com>
2020-03-21 21:44:09 +0300

    feat: add Task6

--- RSSchool_T2/RSSchool_T2/Exercises/4/NSString+Transform.m
+++ RSSchool_T2/RSSchool_T2/Exercises/4/NSString+Transform.m
@@ -3,7 +3,40 @@
 @implementation NSString (Transform)
 
 -(NSString*)transform {
-    return self;
-}
+    NSArray *arr = [self componentsSeparatedByString:@" "];
+    //NSString *test = [NSString new];
+    for (NSUInteger i = 0; i < [arr count]; i++) {
+        for (NSUInteger j = 0; j < [[arr objectAtIndex:i] length]; j++) {
+            if ([self pangram] ) {
+            
+            } else {
+                
+            }
+        }
+        
+        
+    }
+    
+    
+   
+        
+        return self;
+    }
 
-@end
+
+    - (BOOL)pangram {
+        NSMutableSet *set = [[NSMutableSet alloc] init];
+        for (int i = 0; i < self.length; i++) {
+            if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:i]]) {
+                [set addObject:[NSString stringWithFormat:@"%c", [self characterAtIndex:i]]];
+            } else if ([[NSCharacterSet lowercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:i]]) {
+                [set addObject:[NSString stringWithFormat:@"%c", [self characterAtIndex:i]]];
+            }
+        }
+        if (set.count >= 26) {
+            return YES;
+        } else {
+            return NO;
+        }
+    }
+    @end
--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,18 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
+    NSInteger total = 0;
+    NSInteger prev = 1;
+    while (total * prev < [number integerValue] ) {
+        total = total + prev;
+        prev = total - prev;
+        if (total * prev == [number integerValue]){
+            return @[@(prev), @(total), @(1)];
+        }
+        if (total * prev > [number integerValue]) {
+            return @[@(prev), @(total), @(0)];
+        }
+    }
     return @[];
 }
 @end
